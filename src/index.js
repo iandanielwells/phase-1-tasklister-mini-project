a2b73be584b5ad226d679a4c5e700a97b808bb64
iandanielwells <iandanielwells@gmail.com>
2023-01-12 16:40:00 -0500

    completed w/o advanced

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskList = document.getElementById('tasks');
+  const taskForm = document.getElementById('create-task-form');
+  
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById('new-task-description');
+    
+    //creating an li list item, 
+    const newTask = document.createElement('li');
+    newTask.textContent = taskDescription.value;
+
+    //advanced deliverables for delete button
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = "Delete";
+
+    delBtn.addEventListener('click', () => {
+      newTask.remove();
+    })
+
+    newTask.append(delBtn);
+    
+    taskList.append(newTask);
+
+  
+
+    // or taskList.append(newTask, delBtn) then delBtn will appear below the newTask
+  
+  })
+
 });
